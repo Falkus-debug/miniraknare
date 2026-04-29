8594ab0b5e9ceecce766c47af4f847cd3ef022ad
Carl Falck <08falcar@taby.se>
2026-04-29 13:14:55 +0200

    första commit

--- calcer.py
+++ calcer.py
@@ -1,1 +1,51 @@
-print("hej")
+def addition (a,b):
+    return a + b
+
+def subtraktion (a,b):
+    return a-b
+
+def multiplikation(a,b):
+    return a*b
+
+def division(a,b):
+    try:
+        resultat = a / b
+        print(resultat)
+    except ZeroDivisionError:
+        print("du kan inte dividera med 0. använd ett annat tal")
+
+
+while True:
+    operator = input("välj ett räknesätt: + - * /, eller q för att avsluta")
+
+    if operator == "+":
+        tal1 = float(int(input("skriv in det första talet")))
+        tal2 = float(int(input("skriv in det andra talet")))
+        summa = addition(tal1,tal2)
+        print(summa)
+
+    if operator == "-":
+        tal1 = float(int(input("skriv in det första talet")))
+        tal2 = float(int(input("skriv in det andra talet")))
+        differens = subtraktion(tal1,tal2)
+        print(differens)
+
+    if operator == "*":
+        tal1 = float(int(input("skriv in det första talet")))
+        tal2 = float(int(input("skriv in det andra talet")))
+        produkt = multiplikation(tal1,tal2)
+        print(produkt)
+
+    if operator == "/":
+        tal1 = float(int(input("skriv in det första talet")))
+        tal2 = float(int(input("skriv in det andra talet")))
+        kvot = division(tal1,tal2)
+        print (kvot)
+    
+    if operator == 'q':
+        print("programmet avslutas.......")
+        break
+
+
+
+  
